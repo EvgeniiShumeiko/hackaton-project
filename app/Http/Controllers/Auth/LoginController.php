db9511ed3c3bd4ffce25b4165dcb6b1b6de4fe06
vagrant <vagrant@homestead>
2020-05-03 11:19:09 -0500

    [TASK-1] Add login by login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Redis;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,11 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function username()
+    {
+        return 'username';
+    }
 
     /**
      * Create a new controller instance.
