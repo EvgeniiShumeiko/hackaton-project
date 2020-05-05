da94235bf9cc1ae793ece445e64028d60802656c
nikilex <alex_mobi@mail.ru>
2020-05-05 14:04:21 +0500

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -113288,8 +113288,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/virus/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/virus/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/sites/virus/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/sites/virus/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
